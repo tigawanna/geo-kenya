039e619c3b20ad1ec3194b77d7c3b04836b34b60
tigawanna <denniskinuthiaw@gmail.com>
2025-09-28 00:54:59 +0300

    checkpoint

---
+++ __mocks__/ExpoMaterialDynamicColors.ts
@@ -0,0 +1,67 @@
+import type { SystemScheme } from '../modules/expo-material-dynamic-colors/src/ExpoMaterialDynamicColors.types';
+
+const mockSystemScheme: SystemScheme = {
+  primary: '#6750A4',
+  onPrimary: '#FFFFFF',
+  primaryContainer: '#EADDFF',
+  onPrimaryContainer: '#21005D',
+  secondary: '#625B71',
+  onSecondary: '#FFFFFF',
+  secondaryContainer: '#E8DEF8',
+  onSecondaryContainer: '#1D192B',
+  tertiary: '#7D5260',
+  onTertiary: '#FFFFFF',
+  tertiaryContainer: '#FFD8E4',
+  onTertiaryContainer: '#31111D',
+  background: '#FFFBFE',
+  onBackground: '#1C1B1F',
+  surface: '#FFFBFE',
+  onSurface: '#1C1B1F',
+  surfaceVariant: '#E7E0EC',
+  onSurfaceVariant: '#49454F',
+  outline: '#79747E',
+  outlineVariant: '#CAC4D0',
+  inverseSurface: '#313033',
+  inverseOnSurface: '#F4EFF4',
+  inversePrimary: '#D0BCFF',
+};
+
+const mockDarkSystemScheme: SystemScheme = {
+  primary: '#D0BCFF',
+  onPrimary: '#381E72',
+  primaryContainer: '#4F378B',
+  onPrimaryContainer: '#EADDFF',
+  secondary: '#CCC2DC',
+  onSecondary: '#332D41',
+  secondaryContainer: '#4A4458',
+  onSecondaryContainer: '#E8DEF8',
+  tertiary: '#EFB8C8',
+  onTertiary: '#492532',
+  tertiaryContainer: '#633B48',
+  onTertiaryContainer: '#FFD8E4',
+  background: '#1C1B1F',
+  onBackground: '#E6E1E5',
+  surface: '#1C1B1F',
+  onSurface: '#E6E1E5',
+  surfaceVariant: '#49454F',
+  onSurfaceVariant: '#CAC4D0',
+  outline: '#938F99',
+  outlineVariant: '#49454F',
+  inverseSurface: '#E6E1E5',
+  inverseOnSurface: '#313033',
+  inversePrimary: '#6750A4',
+};
+
+const ExpoMaterialDynamicColorsModule = {
+  getSystemTheme: jest.fn(() => ({
+    light: mockSystemScheme,
+    dark: mockDarkSystemScheme,
+  })),
+
+  getSystemThemeAsync: jest.fn(async () => ({
+    light: mockSystemScheme,
+    dark: mockDarkSystemScheme,
+  })),
+};
+
+export default ExpoMaterialDynamicColorsModule;
