728f5f00e472140f926c211881292661bbaa74ae
tigawanna <denniskinuthiaw@gmail.com>
2025-10-14 14:11:56 +0300

    config tweaks

--- app.config.ts
+++ app.config.ts
@@ -13,6 +13,7 @@ const getUniqueIdentifier = () => {
 
   return "com.tigawanna.geokenya";
 };
+type UniqueIDT = ReturnType<typeof getUniqueIdentifier>;
 
 const getAppName = () => {
   if (IS_DEV) {
@@ -26,9 +27,49 @@ const getAppName = () => {
   return { name: "GeoKenya", slug: "geo-kenya" };
 };
 
+const getPlugins = (idt: UniqueIDT) => {
+  const is_production = idt === "com.tigawanna.geokenya";
+  const plugins: ConfigContext["config"]["plugins"] = [
+    "expo-router",
+    "@maplibre/maplibre-react-native",
+    "expo-background-task",
+    [
+      "expo-splash-screen",
+      {
+        image: "./assets/icons/splash-icon-light.png",
+        imageWidth: 200,
+        resizeMode: "contain",
+        backgroundColor: "#534545ff",
+        dark: {
+          image: "./assets/icons/splash-icon-dark.png",
+          backgroundColor: "#390F0F",
+        },
+      },
+    ],
+    [
+      "expo-build-properties",
+      {
+        android: {
+          usesCleartextTraffic: is_production ? false : true, // ? enable HTTP requests
+        },
+        ios: {
+          flipper: true,
+        },
+      },
+    ],
+  ];
+  return plugins;
+};
+
 export default ({ config }: ConfigContext): ExpoConfig => {
   const { name, slug } = getAppName();
-
+  const appIdentifier = getUniqueIdentifier();
+  const plugins = getPlugins(appIdentifier);
+  const is_production = appIdentifier === "com.tigawanna.geokenya";
+  if (is_production) {
+    plugins.push("@react-native-firebase/app");
+    plugins.push("@react-native-firebase/crashlytics");
+  }
   return {
     ...config,
     name: name,
@@ -43,14 +84,14 @@ export default ({ config }: ConfigContext): ExpoConfig => {
       ...config.ios,
       supportsTablet: true,
       infoPlist: {
-        NSAppTransportSecurity: { NSAllowsArbitraryLoads: true }, // ? enable HTTP requests
+        NSAppTransportSecurity: { NSAllowsArbitraryLoads: is_production ? false : true }, // ? enable HTTP requests
       },
       icon: {
         light: "./assets/icons/ios-light.png",
         dark: "./assets/icons/ios-dark.png",
         tinted: "./assets/icons/ios-tinted.png",
       },
-      bundleIdentifier: getUniqueIdentifier(),
+      bundleIdentifier: appIdentifier,
     },
     android: {
       ...config.android,
@@ -59,48 +100,16 @@ export default ({ config }: ConfigContext): ExpoConfig => {
         foregroundImage: "./assets/icons/adaptive-icon.png",
         monochromeImage: "./assets/icons/adaptive-icon.png",
       },
-      googleServicesFile: IS_DEV
-        ? "./google-services.dev.json"
-        : process.env.GOOGLE_SERVICES_JSON ?? "./google-services.json",
+      googleServicesFile: is_production ? "./google-services.json" : undefined,
       edgeToEdgeEnabled: true,
       predictiveBackGestureEnabled: false,
-      package: getUniqueIdentifier(),
+      package: appIdentifier,
     },
     web: {
       output: "static",
       favicon: "./assets/images/favicon.png",
     },
-    plugins: [
-      "expo-router",
-      [
-        "expo-splash-screen",
-        {
-          image: "./assets/icons/splash-icon-light.png",
-          imageWidth: 200,
-          resizeMode: "contain",
-          backgroundColor: "#534545ff",
-          dark: {
-            image: "./assets/icons/splash-icon-dark.png",
-            backgroundColor: "#390F0F",
-          },
-        },
-      ],
-      [
-        "expo-build-properties",
-        {
-          android: {
-            usesCleartextTraffic: true, // ? enable HTTP requests
-          },
-          ios: {
-            flipper: true,
-          },
-        },
-      ],
-      "@maplibre/maplibre-react-native",
-      "expo-background-task",
-      "@react-native-firebase/app",
-      "@react-native-firebase/crashlytics",
-    ],
+    plugins,
     experiments: {
       typedRoutes: true,
       reactCompiler: true,
